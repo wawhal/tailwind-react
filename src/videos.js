89a2bfbeec0f3ab0efb55eb8a4161aa27070083b
Rohit <rohit@hasura.io>
2022-01-04 00:28:46 +0530

    kal

--- src/videos.js
+++ src/videos.js
@@ -29,61 +29,47 @@ export const videos = [{
     channelName: "T-Sreies",
     views: "10M views 10 days ago"
   }, {
-    video: <iframe width="340" height="202" src="https://www.youtube.com/embed/Xxn7lpjNwIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>,
-    img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRuqW22V6Q-FpdA-pFNh7rJKQfP23nMR5WYPoT5LA=s48-c-k-c0x00ffffff-no-rj"></img>,
-    title: "JavaScript Arrays Tutorial in Hindi / urdu",
-    channelName: "Yahoo Baba",
-    views: "50.3M views"
+    video: <iframe width="340" height="202" src="https://www.youtube.com/embed/uS5X6kiwGOA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>,
+    img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/DzaZaTX6gdgjjPI_vkNc2dPbI794UroI9fTAunua0fa7lukDj5NDkjfhS5-w2KXuvXS02r92=s48-c-k-c0x00ffffff-no-rj"></img>,
+    title: "Meri Zindgi Hai Tu Full Video | Jubin Nautiyal | John Abraham",
+    channelName: "T-Sreies",
+    views: "3M views Nov 26, 2021s"
   }, {
-    video: <iframe width="340" height="202" src="https://www.youtube.com/embed/nT-DPEMZvsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>,
+    video: <iframe width="340" height="202" src="https://www.youtube.com/embed/G5CdSCViLWk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> </iframe>,
     img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRsfhph2mDBEgu0bEw0r-8kdNs-S2AVW2gBPLSDqKo=s48-c-k-c0x00ffffff-no-rj"></img>,
-    title: "Meri Zindagi Hai Tu",
-    channelName: "Yahoo Baba",
-    views: "123K views 2 years ago"
+    title: "Barsaat Ke Mausam Mein | Naajayaz | Naseeruddin ",
+    channelName: "Bollywood Dhamaka",
+    views: "42M views Oct 18, 2021"
   }, {
-    video: <iframe width="340" height="202" src="https://www.youtube.com/embed/Xxn7lpjNwIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
-    img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRuqW22V6Q-FpdA-pFNh7rJKQfP23nMR5WYPoT5LA=s48-c-k-c0x00ffffff-no-rj"></img>,
-    title: "JavaScript Arrays Tutorial in Hindi / urdu",
-    channelName: "Yahoo Baba",
-    views: "50.3M views"
+    video: <iframe width="340" height="202" src="https://www.youtube.com/embed/0w62ddeVwGE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+    img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/DzaZaTX6gdgjjPI_vkNc2dPbI794UroI9fTAunua0fa7lukDj5NDkjfhS5-w2KXuvXS02r92=s48-c-k-c0x00ffffff-no-rj"></img>,
+    title: "Tumhe Dillagi Song By Rahat Fateh Ali Khan ",
+    channelName: "T-Sreies",
+    views: "270M views Jun 8, 2016"
   }, {
-  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/nT-DPEMZvsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
-  img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRsfhph2mDBEgu0bEw0r-8kdNs-S2AVW2gBPLSDqKo=s48-c-k-c0x00ffffff-no-rj"></img>,
-  title: "Chand Sifarish - Full Song Audio | Fanaa | Shaan | Kailash Kher ",
-  channelName: "YRF ",
+  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/hoNb6HuNmU0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+  img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/DzaZaTX6gdgjjPI_vkNc2dPbI794UroI9fTAunua0fa7lukDj5NDkjfhS5-w2KXuvXS02r92=s48-c-k-c0x00ffffff-no-rj"></img>,
+  title: "Full Song: KHAIRIYAT (BONUS TRACK) | CHHICHHORE ",
+  channelName: "T-Sreies ",
   views: "2M views Mar 19, 2017"
 }, {
-  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/yfoY53QXEnI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/-qfEOE4vtxE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
   img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s48-c-k-c0x00ffffff-no-rj"></img>,
-  title: "CSS Crash Course",
-  channelName: "Traversory Media",
-  views: "3.3M views"
+  title: "Bootstrap CSS Framework - Full Course for Beginners",
+  channelName: "freeCodeCamp.org",
+  views: "101K  views Dec 7, 2021"
 }, {
-  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/nT-DPEMZvsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
-  img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRsfhph2mDBEgu0bEw0r-8kdNs-S2AVW2gBPLSDqKo=s48-c-k-c0x00ffffff-no-rj"></img>,
-  title: "Meri Zindagi Hai Tu",
-  channelName: "Yahoo Baba",
-  views: "123K views 2 years ago"
-}, {
-  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/Xxn7lpjNwIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
-  img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRuqW22V6Q-FpdA-pFNh7rJKQfP23nMR5WYPoT5LA=s48-c-k-c0x00ffffff-no-rj"></img>,
-  title: "JavaScript Arrays Tutorial in Hindi / urdu",
-  channelName: "Yahoo Baba",
-  views: "50.3M views"
-}, {
-  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/nT-DPEMZvsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-  </iframe>,
-  img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRsfhph2mDBEgu0bEw0r-8kdNs-S2AVW2gBPLSDqKo=s48-c-k-c0x00ffffff-no-rj"></img>,
-  title: "Meri Zindagi Hai Tu",
-  channelName: "Yahoo Baba",
-  views: "123K views 2 years ago"
-}, {
-  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/Xxn7lpjNwIw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-  </iframe>,
-  img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRuqW22V6Q-FpdA-pFNh7rJKQfP23nMR5WYPoT5LA=s48-c-k-c0x00ffffff-no-rj"></img>,
-  title: "JavaScript Arrays Tutorial in Hindi / urdu",
-  channelName: "Yahoo Baba",
-  views: "50.3M views"
+  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/mcdIV1eWjJg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+  img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRvHA4usYha2tDrgCBnc8EKonwF9AgQPjaGl1bG1A=s88-c-k-c0x00ffffff-no-rj"></img>,
+  title: "Bhopal Gas Tragedy | Who was Responsible? | Dhruv Rathee",
+  channelName: "Dhruv Rathee",
+  views: "252K viewsJan 3, 2022"
+},{
+  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/chx9Rs41W6g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
+  img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLRq3jU1DPj1kbW2-B53lHdmiAHYb87EZ0PoZ9oPyQ=s88-c-k-c0x00ffffff-no-rj"></img>,
+  title: "JavaScript | Beginning to Mastery Complete Tutorial (Part 1)",
+  channelName: "Harshit vashisth",
+  views: "26k views"
 }, {
   video: <iframe width="340" height="202" src="https://www.youtube.com/embed/nT-DPEMZvsU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
   </iframe>,
@@ -98,6 +84,20 @@ export const videos = [{
   title: "JavaScript Arrays Tutorial in Hindi / urdu",
   channelName: "Yahoo Baba",
   views: "50.3M views"
+}, {
+  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/c7QYEedjb_o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+  </iframe>,
+  img: <img id="img" class="style-scope yt-img-shadow rounded-full" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLQFBbx9iNh-JQvEo61AHkcDrkbY-Pavaba-y2ck9w=s48-c-k-c0x00ffffff-no-rj"></img>,
+  title: "ABHISHEK UPMANYU |Friends, Crime, & The Cosmos",
+  channelName: "Abhishek Upmanyu",
+  views: "44M views May 8, 2019"
+}, {
+  video: <iframe width="340" height="202" src="https://www.youtube.com/embed/nUUsUAPEjFc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+  </iframe>,
+  img: <img id="img" class="style-scope yt-img-shadow" alt="" width="48" src="https://yt3.ggpht.com/ytc/AKedOLQGJzANzs2ymcXNw3LKBSpQj9EpJojb5a6JY6oPlQ=s48-c-k-c0x00ffffff-no-rj"></img>,
+  title: "Create Responsive Admin Dashboard using Html CSS & Javascript ",
+  channelName: "Online Tutorials",
+  views: "317,146 views"
 },
 
 
